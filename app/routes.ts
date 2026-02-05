eee95e441d14e1303726aa5402eac525c2d9e616
malaguenha <tiesystems@yahoo.co.jp>
2026-02-05 20:54:29 +0900

    The first commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,18 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+
+  // routes in same layout
+  /*
+  layout("layouts/layout-a.tsx", [route("/route-a", "routes/route-a.tsx"),
+    route("/route-b", "routes/route-b.tsx")
+  ]),
+  */
+
+  // routes in sibling layout
+  
+  layout("layouts/layout-a.tsx", [route("/route-a", "routes/route-a.tsx")]),
+  layout("layouts/layout-b.tsx", [route("/route-b", "routes/route-b.tsx")]),
+
+] satisfies RouteConfig;
